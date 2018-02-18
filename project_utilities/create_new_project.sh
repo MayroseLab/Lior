f9959ea78afd3a471abef1e3554e161e7c4e6b1f
Lior Glick <liorglic@jekyl.tau.ac.il>
2018-02-18 12:18:23 +0200

    utility shell to create new project templates

---
+++ project_utilities/create_new_project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+projectName=$1
+
+baseDir="/groups/itay_mayrose/liorglic/Projects"
+projectDir="$baseDir/$projectName"
+mkdir $projectDir
+mkdir "$projectDir/R"
+mkdir "$projectDir/data"
+mkdir "$projectDir/doc"
+mkdir "$projectDir/figs"
+mkdir "$projectDir/output"
+mkdir "$projectDir/notebooks"
+createDate=`date`
+echo "Project $projectName - created $createDate" > "$projectDir/README"
