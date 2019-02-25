bd39fe791b7a675dab4bbac34fc97d4bd443fa39
liorglic <liorglic@mail.tau.ac.il>
2019-02-25 16:54:56 +0200

    add some features to fasta split script

--- tomato_pan_genome/python/split_fasta_records.py
+++ tomato_pan_genome/python/split_fasta_records.py
@@ -1,29 +1,56 @@
-# Take a fasta file and create a new one in which every record is
-# split into chunks of a user-defined size. New record IDs are:
-# ><original id>_<start>_<end>
+"""
 
-from __future__ import print_function
+"""
+
+from __future__ import print_function, division
 from Bio import SeqIO
-import sys
+import argparse
+import os
+from math import ceil
 
-in_fasta = sys.argv[1]
-out_fasta = sys.argv[2]
-chunk_size = int(sys.argv[3])
+parser = argparse.ArgumentParser()
+parser.add_argument('in_fasta', help="input_fasta file")
+parser.add_argument('out', help="Output fasta or output dir (in break mode)")
+parser.add_argument('--chunk_size', help="chunk size in bp", type=int)
+parser.add_argument('--n_chunks', help="number of chunks to break_mode into", type=int)
+parser.add_argument('--break_mode', default=False, action='store_true', help="write each chunk to a separate file")
+args = parser.parse_args()
 
-with open(in_fasta) as f, open(out_fasta,'w') as fo:
+if not (args.chunk_size or args.n_chunks) or (args.chunk_size and args.n_chunks):
+  exit("Must specify either chunk size or n chunks, but not both")
+if args.break_mode and not os.path.isdir(args.out):
+  exit("Output must be a directory when using break_mode mode")
+
+# if number of chunks was given, get total sequence
+# length and calculate required chunk size
+if args.n_chunks:
+  tot_len = 0
+  with open(args.in_fasta) as f:
+    for rec in SeqIO.parse(f,'fasta'):
+      rec_seq = rec.seq
+      tot_len += len(rec_seq)
+  chunk_size = int(ceil(tot_len // args.n_chunks))
+else:
+  chunk_size = args.chunk_size
+
+if not args.break_mode:	# single output file
+  fo = open(args.out,'w')
+with open(args.in_fasta) as f:
   for rec in SeqIO.parse(f,'fasta'):
     rec_id =  rec.id
     rec_seq = rec.seq
     seq_len = len(rec_seq)
-    if seq_len < chunk_size:
-      chunk_id = "%s_0_%s" %(rec_id, seq_len)
-      print(">%s" % chunk_id, file=fo)
-      print(rec_seq, file=fo)
-      continue
     for i in range(0,seq_len,chunk_size):
       start = i
       end = min(i + chunk_size, seq_len)
       chunk_id = "%s_%s_%s" %(rec_id,start,end)
+      chunk_seq = rec_seq[start:end]
+      if args.break_mode:
+        out_file = "%s/%s.fasta" %(args.out, chunk_id)
+        fo = open(out_file,'w')
       print(">%s" % chunk_id, file=fo)
-      print(rec_seq[start:end], file=fo)
-   
+      print(chunk_seq, file=fo)
+      if args.break_mode:
+        fo.close()
+if not args.break_mode:
+  fo.close()
