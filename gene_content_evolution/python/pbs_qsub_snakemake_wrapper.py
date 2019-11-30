3352102426ab1ec02495b79f060f8e347b802cd9
liorglic <liorglic@mail.tau.ac.il>
2019-11-30 22:11:06 +0200

    include sample name in job/log name

---
+++ gene_content_evolution/python/pbs_qsub_snakemake_wrapper.py
@@ -0,0 +1,27 @@
+#!python
+#!/usr/bin/env python3
+import os
+import sys
+
+from snakemake.utils import read_job_properties
+
+jobscript = sys.argv[1]
+job_properties = read_job_properties(jobscript)
+
+if "nodes" in job_properties["params"]:
+  nodes = job_properties["params"]["nodes"]
+else:
+  nodes = 1
+if "ppn" in job_properties["params"]:
+  ppn = job_properties["params"]["ppn"]
+else:
+  ppn = 1
+rule_name = job_properties["rule"]
+if 'sample' in job_properties["wildcards"]:
+  base = job_properties["wildcards"]['sample']
+else:
+  base = "all_samples"
+queue = job_properties["params"]["queue"]
+priority = job_properties["params"]["priority"]
+logs_dir = job_properties["params"]["logs_dir"]
+os.system("qsub -N {base}_{rule} -p {priority} -q {queue} -l nodes={nodes}:ppn={ppn} -o {logs_dir}/{base}_{rule}.out -e {logs_dir}/{base}_{rule}.err {script}".format(base=base, rule=rule_name, priority=priority, queue=queue, nodes=nodes, ppn=ppn, logs_dir=logs_dir, script=jobscript))
