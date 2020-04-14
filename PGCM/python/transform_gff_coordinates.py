40e8bcd0ebef8df2ab9a9b06496fc9691453ea1e
liorglic <liorglic@mail.tau.ac.il>
2020-04-14 11:44:23 +0300

    new scripts for gff processing

---
+++ PGCM/python/transform_gff_coordinates.py
@@ -0,0 +1,35 @@
+"""
+Takes a gff file, where record names have the form
+X__start-end and converts to record name X with start
+and end columns adjusted.
+This is useful when merging split gff files.
+"""
+from __future__ import print_function
+import sys
+
+in_gff = sys.argv[1]
+out_gff = sys.argv[2]
+
+with open(in_gff) as f, open(out_gff,'w') as fo:
+  for line in f:
+    line = line.strip()
+    if line.startswith('#'):
+      print(line,file=fo)
+      continue
+    fields = line.split('\t')
+    if len(fields) != 9:
+      print(line,file=fo)
+      continue
+    if fields[2] == "contig":
+      continue
+    if '__' not in fields[0]:
+      print(line,file=fo)
+      continue
+    rec_name_with_coords = fields[0]
+    rec_name_split = fields[0].split('__')
+    real_rec_name = rec_name_split[0]
+    start_on_real_rec = int(rec_name_split[1].split('-')[0])
+    fields[0] = real_rec_name
+    fields[3] = str(int(fields[3]) + start_on_real_rec)
+    fields[4] = str(int(fields[4]) + start_on_real_rec)
+    print('\t'.join(fields), file=fo)
