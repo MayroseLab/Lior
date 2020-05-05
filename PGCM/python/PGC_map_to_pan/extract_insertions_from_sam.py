8ee3f5fbaa518b95f79317a2b96d794066bd1ad3
liorglic <liorglic@mail.tau.ac.il>
2020-05-05 10:06:42 +0300

    avoid crashing when sequence is not available

--- PGCM/python/PGC_map_to_pan/extract_insertions_from_sam.py
+++ PGCM/python/PGC_map_to_pan/extract_insertions_from_sam.py
@@ -20,7 +20,7 @@ for aln in sam:
     continue
   start = 0
   for ct in cigar_tuples:
-    if ct[0] == 1 and ct[1] > min_insert:	# insertion (I) operation
+    if ct[0] == 1 and ct[1] > min_insert and aln.query_sequence:	# insertion (I) operation
       insert_seq = aln.query_sequence[start:start+ct[1]]
       seq_name = "%s_%s_insert" %(aln.query_name, start)
       print('>%s' % seq_name)
