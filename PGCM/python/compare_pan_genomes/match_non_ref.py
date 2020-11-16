07ea1457baa2f6608a356571ae5d64e72bb62430
liorglic <liorglic@mail.tau.ac.il>
2020-11-16 17:41:27 +0200

    new version

--- PGCM/python/compare_pan_genomes/match_non_ref.py
+++ PGCM/python/compare_pan_genomes/match_non_ref.py
@@ -15,6 +15,7 @@ TSV file with the matches pairs and their respective weight
 
 from __future__ import print_function, division
 from networkx import bipartite, matching
+from networkx.algorithms.bipartite.matching import minimum_weight_full_matching
 import argparse
 import csv
 
@@ -63,9 +64,11 @@ def parse_blast6(blast6, weight_param):
       res[(row['qseqid'], row['sseqid'])] = max(w,float(row[weight_param]))
   return res
 
+print("Parsing BLAST files...")
 set1_vs_set2_weights = parse_blast6(args.set1_vs_set2_blast, args.weight_param)
 set2_vs_set1_weights = parse_blast6(args.set2_vs_set1_blast, args.weight_param)
 
+print("Assigning edge weights...")
 # go over all BG edges and assign weights
 for edge in bg.edges:
   q = set1_proteins[edge[0]]
@@ -76,18 +79,24 @@ for edge in bg.edges:
   if (s,q) in set2_vs_set1_weights:
     bidirectional_weight += set2_vs_set1_weights[(s,q)]
   bidirectional_weight /= 2
+  bidirectional_weight *= -1 # b/c the function looks for the minimum weight matching
   bg.edges[edge]['weight'] = bidirectional_weight
 
+print("Finding max weight matching...")
 # find max weight matching
-mw = matching.max_weight_matching(bg,maxcardinality=True)
+mw = minimum_weight_full_matching(bg)
 
+print("Writing out results...")
 # print out matches
 with open(args.out_tsv, 'w') as fo:
   print("%s\t%s\t%s weight" %(args.set1_name, args.set2_name, args.weight_param), file=fo)
   for match in mw:
-    match = (min(match), max(match))
-    match_weight =  bg.edges[match]['weight']
+    source, target = match, mw[match]
+    if target < source:
+      # each match appears in both directions in the output dict
+      continue
+    match_weight = -1 * bg[source][target]['weight']
     if match_weight >= args.min_weight:
-      q_name = set1_proteins[match[0]]
-      s_name = set2_proteins[match[1]]
+      q_name = set1_proteins[source]
+      s_name = set2_proteins[target]
       print("%s\t%s\t%s" %(q_name, s_name, match_weight), file=fo)
